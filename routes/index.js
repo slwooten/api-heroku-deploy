e00cb9bb1bf8e403a3242bf8c20094a9ca01d704
Seth Wooten <slwooten14@gmail.com>
2022-04-01 12:00:08 -0400

    setup server and routes

---
+++ routes/index.js
@@ -0,0 +1,43 @@
+const router = require('express').Router();
+
+router.get('/api/travellers', (req, res) => {
+  res.json({});
+});
+
+router.post('/api/travellers', (req, res) => {
+  res.json({});
+});
+
+router.get('/api/travellers/:id', (req, res) => {
+  res.json({});
+});
+
+router.delete('/api/travellers/:id', (req, res) => {
+  res.json({});
+});
+
+router.get('/api/locations', (req, res) => {
+  res.json({});
+});
+
+router.post('/api/locations', (req, res) => {
+  res.json({});
+});
+
+router.get('/api/locations/:id', (req, res) => {
+  res.json({});
+});
+
+router.delete('/api/locations/:id', (req, res) => {
+  res.json({});
+});
+
+router.post('/api/trips', (req, res) => {
+  res.json({});
+});
+
+router.delete('/api/trips/:id', (req, res) => {
+  res.json({});
+});
+
+module.exports = router;
